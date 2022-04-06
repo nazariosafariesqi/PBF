7f9ab6942b5676d16a0d0e28f6572e771aeb9d07
Nazario Safariesqi <nazariotyo@gmail.com>
2022-04-06 20:21:22 +0700

    Praktikum 7

---
+++ Jobsheet7/praktikum/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import './App.css'
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <ClickCounter/>
+        <HoverCounter/>
+      </div>
+    )
+  }
+}
+
+export default App;
